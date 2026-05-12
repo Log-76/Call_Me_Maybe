a4b465b97c45d08fff35f1d08831914762d5783d
Logann Leriche <lleriche@c1r6p12.42lehavre.fr>
2026-05-12 12:52:09 +0200

    parse ajoute des get pour les recuperer chaque info

--- parse.py
+++ parse.py
@@ -5,3 +5,12 @@ class Parse(BaseModel):
     fonct: str = Field(strict=str)
     input_file: str = Field(strict=str)
     ouput_file: str = Field(strict=str)
+
+    def get_fonct(self):
+        return self.fonct
+
+    def get_input_file(self):
+        return self.input_file
+
+    def get_ouput_file(self):
+        return self.ouput_file
