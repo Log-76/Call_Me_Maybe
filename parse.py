391945930b1bbb1c481614c523f9b7f9d30e8be6
Logann Leriche <lleriche@c1r6p12.42lehavre.fr>
2026-05-12 15:59:47 +0200

    .json

--- parse.py
+++ parse.py
@@ -1,10 +1,22 @@
 from pydantic import BaseModel, Field
+import json
 
 
 class Parse(BaseModel):
     fonct: str = Field(strict=True)
     input_file: str = Field(strict=True)
     ouput_file: str = Field(strict=True)
+
+    def fonction_def(self):
+        try:
+            with open(self.fonct, "r") as f:
+                return json.load(f)
+        except FileNotFoundError:
+            print("error file not found")
+        except json.JSONDecodeError as e:
+            print(e)
+        except Exception:
+            print("error")
 
     def get_fonct(self):
         return self.fonct
